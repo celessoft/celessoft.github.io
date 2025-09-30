4d9c55dc306045c4f734f8e8682d959d16a21221
celessoft <celessoft.team@gmail.com>
2025-09-30 17:34:36 +0900

    google search console 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="5OdKzjEoKNd3admJWShOy8FlLvga3n2eDo4oANB6jNI" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
